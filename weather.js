046e8a5dd244415bce89cf5bfe4682bf6972602b
imtiaz4616 <imtiazuddinedu@gmail.com>
2023-10-16 15:47:11 +0600

    update

--- weather.js
+++ weather.js
@@ -14,9 +14,6 @@ async function checkWeather(city){
     }
     var data = await response.json();
 
-    console.log(data); 
-
-
     document.querySelector(".city").innerHTML=data.name;
     
     document.querySelector(".temp").innerHTML= Math.round(data.main.temp )+ "°c";
